5ff272ee9417822816dcbe2ecd909c9eee8ae77c
ASArutyunyan <aleks_pochta_krd@mail.ru>
2024-10-10 22:20:01 +0300

    сделал способ загрузить raw файл. использовал "a[href*='/raw/']"

--- src/test/java/files/SelenideFilesTest.java
+++ src/test/java/files/SelenideFilesTest.java
@@ -1,19 +1,31 @@
 package files;
 
 import org.junit.jupiter.api.Test;
-
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SelenideFilesTest {
 
     @Test
-    void selenideDownloadTest() throws FileNotFoundException {
-        open("href=https://github.com/junit-team/junit5/raw/refs/heads/main/README.md");
-        File downloadedFile = $(".raw-button").download();
+    void selenideDownloadTest() throws Exception{
+        open("https://github.com/junit-team/junit5/blob/main/README.md");
+        sleep(7000);
+        File downloadedFile =  $("a[href*='/raw/']").download();
+        InputStream is = new FileInputStream(downloadedFile);
+        byte[] bytes = is.readAllBytes();
+        String textContent = new String(bytes, StandardCharsets.UTF_8);
+        assertThat(textContent).contains("This repository is the home of ");
+        is.close();
+
+        // Выводим путь к загруженному файлу
+        System.out.println("File downloaded to: " + downloadedFile.getAbsolutePath());
 
     }
 }
