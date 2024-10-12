55eb767d8e285caf0d64207e76d8cc6641d63fbe
ASArutyunyan <aleks_pochta_krd@mail.ru>
2024-10-12 17:35:40 +0300

    добавил проверку pdf/ если запустить с жуком то можно с помощью калькулятора проверить пдф файл

--- src/test/java/files/FileParsingTest.java
+++ src/test/java/files/FileParsingTest.java
@@ -1,12 +1,25 @@
 package files;
 
+import com.codeborne.pdftest.PDF;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import static com.codeborne.pdftest.assertj.Assertions.assertThat;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class FileParsingTest {
 
     @Test
-    void pdfParseTest() {
-        open("https://")
-
+    void pdfParseTest() throws Exception {
+        open("https://junit.org/junit5/docs/current/user-guide/");
+        File downloadPdf = $("a[href='junit-user-guide-5.11.2.pdf']").download();
+        PDF content = new PDF(downloadPdf);
+        assertThat(content.author).contains("Sam Brannen");
+        }
     }
-}
+
